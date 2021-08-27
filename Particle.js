8dafd175d97a86fac87b8211034040b18a0ca2f2
VaishviPanchal <78603593+VaishviPanchal@users.noreply.github.com>
2021-08-28 00:20:31 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,13 +1,13 @@
 class Particle {
-    constructor() {
+    constructor(x,y) {
         var options = {
             restitution: 1,
             friction: 0,
-            isStatic:true
+            isStatic:false
         }
         this.r = 10;
-        this.body = Bodies.circle(100, random(10,790), this.r, options);
-        //this.color = this.color(random(0,225),random(0,225),random(0,225))
+        this.body = Bodies.circle(x, y, this.r, options);
+        //this.color = color(random(0,225),random(0,225),random(0,225))
         World.add(world, this.body);
     }
     display() {
